0914517a8fdcd0523d3f4db446983bfa5e3743b3
patidardeep <patidardeepak327@gmail.com>
2023-04-29 11:23:25 +0530

    todolist

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,70 @@
-import React, { useState } from 'react'
+import { nanoid } from 'nanoid'
+import  { useState } from 'react'
 
 const index = () => {
 
-let [uname, setUname] = useState("Deepak")
-const [myclock, setMyclock] = useState(new Date ().toLocaleTimeString());
+const [title, setTitle] = useState("")
+const [desc, setDesc] = useState("")
+const [tasks, setTasks] = useState([])
 
 
-setInterval(()=>{
-  setMyclock(new Date().toLocaleTimeString())
-},1000)
 
-const changehandler = () =>{
-  console.log("change name")
-  setUname(" Deepak Patidar")
+const createTaskHandler = (e) => {
+  e.preventDefault();
+  const todo = {
+    id:nanoid(),
+    title,
+    desc,
+  }
+  console.log(todo)
+  setTasks([...tasks,todo])
+  setTitle("")
+  setDesc("")
+}
+
+const DeleteHandler = (id) => {
+  const copyTasks = tasks.filter((task)=>task.id !==id)
+  setTasks(copyTasks);
+}
+
+let taskslist = (
+  <h4>no pending task....</h4>
+)
+if(tasks.length>0){
+  taskslist = tasks.map((task,index)=>(
+    <li key={task.id}>
+{task.title}
+<i onClick={()=>DeleteHandler(task.id)}
+
+className="text-danger ri-delete-bin-2-line"
+>
+ 
+</i>
+    </li>
+  ))
 }
 
   return (
     <div>
-      <h1>{uname}</h1>
-      <h2>{myclock}</h2>
-      <button onClick={changehandler}>Change Name</button>
+      <form onSubmit={createTaskHandler}>
+        <input
+         type="text" 
+         value={title}
+         placeholder='Title'
+         onChange={(e)=>setTitle(e.target.value)}
+         />
+          <input
+         type="text" 
+         value={desc}
+         placeholder='desc'
+         onChange={(e)=>setDesc(e.target.value)}
+         />
+         <button>Create Task</button>
+      </form>
+      <hr />
+      <ul>
+    {taskslist}
+      </ul>
     </div>
   )
 }
