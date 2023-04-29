fdd0c876c84b8e278f17305dfcdc73b099ea99da
patidardeep <patidardeepak327@gmail.com>
2023-04-29 09:59:56 +0530

    useState Live clock

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,12 @@ import React, { useState } from 'react'
 const index = () => {
 
 let [uname, setUname] = useState("Deepak")
+const [mydate, setMydate] = useState(new Date ().toLocaleTimeString());
 
+
+setInterval(()=>{
+  setMydate(new Date().toLocaleTimeString())
+},1000)
 
 const changehandler = () =>{
   console.log("change name")
@@ -13,6 +18,7 @@ const changehandler = () =>{
   return (
     <div>
       <h1>{uname}</h1>
+      <h2>{mydate}</h2>
       <button onClick={changehandler}>Change Name</button>
     </div>
   )
