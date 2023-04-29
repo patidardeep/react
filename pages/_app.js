f4b3b3f9300cfd3de354318c638e45fc29ee2aba
patidardeep <patidardeepak327@gmail.com>
2023-04-29 09:51:41 +0530

    useState

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,8 @@
-import '@/styles/globals.css'
+import "remixicon/fonts/remixicon.css";
+import "@/node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
+  
 }
